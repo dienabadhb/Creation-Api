b5df57fece677835702d8eafcc8beac06998a19a
vanellazerr <v_effafegue@etu-webschoolfactory.fr>
2026-01-07 21:37:51 +0100

    gestion automatique

---
+++ middlewares/format.middleware.js
@@ -0,0 +1,32 @@
+import { create } from "xmlbuilder2";
+import { stringify } from "csv-stringify/sync";
+import formatConfig from "../config/format.js";
+
+function getResourceName(req, isCollection) {
+  const base = req.path.split("/").filter(Boolean).pop();
+  if (!base) return "data";
+  return isCollection ? base : base.slice(0, -1);
+}
+
+export default (req, res, next) => {
+  res.sendFormatted = (data) => {
+    const format = req.query.format || formatConfig.defaultFormat;
+    const isCollection = Array.isArray(data.data);
+    const rootName = getResourceName(req, isCollection);
+
+    if (format === "xml") {
+      return res.type("xml").send(
+        create({ [rootName]: data.data }).end({ prettyPrint: true })
+      );
+    }
+
+    if (format === "csv") {
+      return res
+        .type("text/csv")
+        .send(stringify(data.data, { header: true }));
+    }
+
+    res.json(data);
+  };
+  next();
+};
